6067b34919e59a3413ca0c0b5db614db020f2818
71200573 - Jonathan Yomaki <78341279+JonathanYomaki0205@users.noreply.github.com>
2025-09-04 11:05:54 +0700

    Update kontak.blade.php
    link kontak

--- resources/views/partials/kontak.blade.php
+++ resources/views/partials/kontak.blade.php
@@ -2,6 +2,7 @@
     <div class="kontak-container">
         <h3 class="section-title">Butuh Bantuan?</h3>
         <p>Tim customer service kami siap bantu kamu 24/7. Jangan ragu untuk hubungi kami!</p>
-        <a href=""  class="btn-gold" >Chat Sekarang</a>
+        <a href="https://api.whatsapp.com/send/?phone=%2B6281904104102&text&type=phone_number&app_absent=0" class="btn-gold" target="_blank" rel="noopener">Chat Sekarang</a>
     </div>
 </section>
+
