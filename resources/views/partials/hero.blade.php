4966283f11d638172040763eeb50d07fcb20b7c7
stevantarigan <stevantrgn@gmail.com>
2025-09-08 08:29:27 +0700

    Update Hero

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,9 +1,18 @@
 <section class="hero">
     <div class="hero-content">
-        <h3 class="section-title">Investasi Emas & Perak Masa Kini</h3>
-        <p>Dapatkan harga real-time dan produk bersertifikat resmi dari Glory Gold. Investasi aman dan menguntungkan!
-        </p>
-        <a href="{{ route('katalog.emas') }}" class="btn-gold"> Temukan Koleksi Emas </a>
-        <a href="{{ route('katalog.perak') }}" class="btn-silver1"> Temukan Koleksi Perak </a>
+        <h1 class="hero-title">
+            <span class="gold-text">Investasi Emas</span> &
+            <span class="silver-text">Perak Masa Kini</span>
+        </h1>
+        <p class="hero-description">Dapatkan harga real-time dan produk bersertifikat resmi dari Glory Gold. Investasi
+            aman dan menguntungkan!</p>
+        <div class="btn-container">
+            <a href="{{ route('katalog.emas') }}"class="btn-gold">
+                <i class="fas fa-coins"></i> Temukan Koleksi Emas
+            </a>
+            <a href="{{ route('katalog.perak') }}" class="btn-silver1">
+                <i class="fas fa-ring"></i> Temukan Koleksi Perak
+            </a>
+        </div>
     </div>
 </section>
