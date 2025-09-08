b44bdf1ada36f780f6112e1e3d2197d2f6b59fcc
stevantarigan <stevantrgn@gmail.com>
2025-09-08 09:06:46 +0700

    Kontak & Footer Update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,12 @@
-<footer class="bg-gray-800 text-gray-400 py-6 text-center">
-  <p>&copy; 2025 Glory Gold. Semua Hak Dilindungi.</p>
+<footer class="footer-bar">
+    <div class="footer-content">
+        <p class="footer-copy">&copy; 2025 Glory Gold. Semua Hak Dilindungi.</p>
+        <div class="footer-social">
+            <a href="https://facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="https://linkedin.com/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://youtube.com/" target="_blank"><i class="fab fa-youtube"></i></a>
+        </div>
+    </div>
 </footer>
