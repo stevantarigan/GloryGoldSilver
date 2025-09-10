fab67ec690f0482df57b9f5d6407cb5fe358c15c
stevantarigan <stevantrgn@gmail.com>
2025-09-10 10:19:18 +0700

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="footer-content">
         <p class="footer-copy">&copy; 2025 Glory Gold. Semua Hak Dilindungi.</p>
         <div class="footer-social">
-            <a href="https://facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
-            <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
-            <a href="https://instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
-            <a href="https://linkedin.com/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-            <a href="https://youtube.com/" target="_blank"><i class="fab fa-youtube"></i></a>
+            <a href="javascript:void(0)" onclick="location.reload()"><i class="fab fa-facebook-f"></i></a>
+            <a href="javascript:void(0)" onclick="location.reload()"><i class="fab fa-twitter"></i></a>
+            <a href="javascript:void(0)" onclick="location.reload()"><i class="fab fa-instagram"></i></a>
+            <a href="javascript:void(0)" onclick="location.reload()"><i class="fab fa-linkedin-in"></i></a>
+            <a href="javascript:void(0)" onclick="location.reload()"><i class="fab fa-youtube"></i></a>
         </div>
     </div>
 </footer>
