4dcd14f2687f79c7f016e3c83e00bd90b170cd73
stevantarigan <stevantrgn@gmail.com>
2025-09-11 12:17:33 +0700

    Update Logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <div class="logo">
-        <a href="{{ url('/') }}" class="logo-link">
-            <i class="fas fa-crown logo-icon"></i>
+        <a href="{{ url('/') }}" class="logo-link flex items-center">
+            <img src="{{ asset('images/logo3.png') }}" alt="Glory Gold Logo" class="logo-img" style="height: 40px; margin-right: 0px;">
             <h1>Glory Gold</h1>
         </a>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ Route::get('/katalog/emas', [ProductController::class, 'emas'])->name('katalog.e
 Route::get('/katalog/perak', [ProductController::class, 'perak'])->name('katalog.perak');
 
 
-// katalog emas → ambil dari controller
-Route::get('/emas', [ProductController::class, 'emas'])->name('katalog.emas');
+// // katalog emas → ambil dari controller
+// Route::get('/emas', [ProductController::class, 'emas'])->name('katalog.emas');
 
-// katalog perak → ambil dari controller
-Route::get('/perak', [ProductController::class, 'perak'])->name('katalog.perak');
+// // katalog perak → ambil dari controller
+// Route::get('/perak', [ProductController::class, 'perak'])->name('katalog.perak');
 
