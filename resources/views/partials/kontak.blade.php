73af656679b7de37a9e8c6f537e0493a76c28b1a
JonathanYomaki71200573 <jonathan.andreas@ti.ukdw.ac.id>
2025-09-11 09:50:38 +0700

    Update kontak.blade.php

--- resources/views/partials/kontak.blade.php
+++ resources/views/partials/kontak.blade.php
@@ -5,7 +5,7 @@
             Tim customer service kami siap bantu kamu <strong>24/7</strong>. <br>
             Jangan ragu untuk hubungi kami!
         </p>
-        <a href="https://api.whatsapp.com/send/?phone=%2B6281904104102&text&type=phone_number&app_absent=0"
+        <a href="https://api.whatsapp.com/send/?phone=6281904104102"
             class="btn-gold btn-whatsapp" target="_blank" rel="noopener">
             <i class="fab fa-whatsapp"></i> Chat via WhatsApp
         </a>
