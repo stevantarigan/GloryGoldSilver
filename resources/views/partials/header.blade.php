41527c05627456efb79aa7530d0f1b9d1c5949d4
stevantarigan <stevantrgn@gmail.com>
2025-09-16 09:17:59 +0700

    Update Mobile Layout

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,7 +11,7 @@
     @if (!Request::routeIs('artikel.index') && !Request::routeIs('artikel.show'))
         <nav>
             <div class="nav-item">
-                <a href="#produk">Produk <i class="fas fa-chevron-down"></i></a>
+                <a href="#">Produk <i class="fas fa-chevron-down"></i></a>
                 <div class="dropdown">
                     <a href="{{ route('katalog.emas') }}">Emas</a>
                     <a href="{{ route('katalog.perak') }}">Perak</a>
